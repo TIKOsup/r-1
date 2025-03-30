8051adb3bc75a4288e6ead9587ee65a9777fb51e
TIKOsup <temirlan.kystaubayev@gmail.com>
2025-03-30 15:29:10 +0500

    feat: add todo create form

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import TodoList from "@/components/TodoList";
 
 export default function App() {
   return (
-    <>
-      <div className="m-3">
-        <TodoList />
-      </div>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button className="cursor-pointer">Destructive</Button>
-      </div>
-    </>
+    <div className="flex flex-col items-center space-y-3 min-h-svh m-5">
+      <TodoList />
+      <Button className="cursor-pointer">Destructive</Button>
+    </div>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,38 @@
 import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { todo } from "@/data.js";
 
 export default function TodoList() {
-  const [todoItems, setTodoItems] = useState(
-    todo
-  );
+  const [todoItems, setTodoItems] = useState(todo);
+  const [inputText, setInputText] = useState("");
+
+  function handleChangeInput(event) {
+    setInputText(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setTodoItems([
+      ...todoItems,
+      {
+        id: todoItems.length,
+        text: inputText,
+        done: false
+      }
+    ]);
+    setInputText("");
+  }
 
   function handleChangeTodo(id) {
     const nextTodoItems = todoItems.map((item) => {
@@ -23,16 +50,32 @@ export default function TodoList() {
   }
 
   return (
-    <div className="flex flex-col space-y-2">
-      {todoItems.map(item => <Todo data={item} onChange={handleChangeTodo} key={item.id} />)}
-    </div>
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Todo ist</CardTitle>
+        <CardDescription>Basic todo list for task tracking</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col space-y-5">
+        <form onSubmit={handleSubmit} className="flex flex-row space-x-2">
+          <Label htmlFor="task-name" className="sr-only">Task</Label>
+          <Input id="task-name" type="text" placeholder="Task" onChange={handleChangeInput} />
+          <Button type="submit">Add</Button>
+        </form>
+        <ul className="space-y-2">
+          {todoItems.map(item => <Todo data={item} onChange={handleChangeTodo} key={item.id} />)}
+        </ul>
+      </CardContent>
+      <CardFooter>
+        <p>Footer</p>
+      </CardFooter>
+    </Card>
   )
 }
 
 
 function Todo({ data, onChange }) {
   return (
-    <div className="flex items-center space-x-2">
+    <li className="flex items-center space-x-2">
       <Checkbox id={"todo-item-" + data.id} checked={data.done} onCheckedChange={() => onChange(data.id)} />
       <label
         htmlFor={"todo-item-" + data.id}
@@ -40,6 +83,6 @@ function Todo({ data, onChange }) {
       >
         {data.text}
       </label>
-    </div>
+    </li>
   )
 }
