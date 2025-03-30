bfaeca0ae0105b2cb16c114a7337b9fceda0c133
TIKOsup <temirlan.kystaubayev@gmail.com>
2025-03-30 13:05:45 +0500

    feat: add basic checkbox state change

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
+import { todo } from "@/data.js";
 
 export default function TodoList() {
+  const [todoItems, setTodoItems] = useState(
+    todo
+  );
+
+  function handleChangeTodo(id) {
+    const nextTodoItems = todoItems.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          done: !item.done
+        }
+      } else {
+        return item;
+      }
+    });
+
+    setTodoItems(nextTodoItems);
+  }
+
   return (
     <div className="flex flex-col space-y-2">
-      <Todo />
-      <Todo />
-      <Todo />
+      {todoItems.map(item => <Todo data={item} onChange={handleChangeTodo} key={item.id} />)}
     </div>
   )
 }
 
 
-function Todo() {
+function Todo({ data, onChange }) {
   return (
     <div className="flex items-center space-x-2">
-      <Checkbox id="todo-item-0" />
+      <Checkbox id={"todo-item-" + data.id} checked={data.done} onCheckedChange={() => onChange(data.id)} />
       <label
-        htmlFor="todo-item-0"
+        htmlFor={"todo-item-" + data.id}
         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
       >
-        Do back training
+        {data.text}
       </label>
     </div>
   )
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const todo = [
+  {
+    id: 0,
+    text: "Clear floor",
+    done: false
+  },
+  {
+    id: 1,
+    text: "Breath a fresh air in the park",
+    done: true
+  },
+  {
+    id: 2,
+    text: "Do 20 push-ups",
+    done: false
+  },
+]
